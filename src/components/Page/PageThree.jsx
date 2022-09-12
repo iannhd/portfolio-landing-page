6519354fd4c9facb4ffaf49f8dd01c16e8cda7e9
Ihdhar Nur <in.hadyan@gmail.com>
2022-09-12 16:49:25 +0700

    testing auto-deploy

--- src/components/Page/PageThree.jsx
+++ src/components/Page/PageThree.jsx
@@ -6,6 +6,7 @@ const PageThree = () => {
         <div className="row">
             <div className="col">
                 <h2>Porfolio</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint esse, hic deserunt dicta iusto architecto?</p>
             </div>
         </div>
     </div>
